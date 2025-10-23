795d205557940a09cdeaf02fa7da0acad8dd323b
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-10-23 20:47:49 +0100

    Update DataPurchase.tsx

--- src/pages/DataPurchase.tsx
+++ src/pages/DataPurchase.tsx
@@ -53,7 +53,7 @@ const DataPurchase = () => {
       return;
     }
 
-    if (bpcCode !== "BPC166242722") {
+    if (bpcCode !== "BPC55262527") {
       toast({
         variant: "destructive",
         description: "Invalid BPC code. Please enter a valid code.",
